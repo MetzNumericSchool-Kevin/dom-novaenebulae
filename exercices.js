532c865434030ed677a70c1cf2d2bb4f7f2b7256
Lucas Olivarez <74412774+novaenebulae@users.noreply.github.com>
2025-02-12 09:08:43 +0100

    Add exercice 2

--- exercices.js
+++ exercices.js
@@ -3,3 +3,11 @@
 const mainTitle = document.querySelector('h1');
 
 console.log(mainTitle.textContent);
+
+// Exercice 2 : Des informations manquent ! : 
+
+const descriptionDiv = document.getElementById('description_boutique');
+
+const descriptionElement = document.createElement('p')
+descriptionElement.textContent = "Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l'acheter.";
+descriptionDiv.appendChild(descriptionElement);
