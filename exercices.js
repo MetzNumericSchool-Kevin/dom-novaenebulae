c07b395f29b15b15c1b3cc10fc20eb5de894a423
Lucas Olivarez <74412774+novaenebulae@users.noreply.github.com>
2025-02-12 09:58:46 +0100

    Add exercice 5

--- exercices.js
+++ exercices.js
@@ -23,3 +23,28 @@ const callButton = document.getElementById("call_archibald");
 callButton.addEventListener("click", () => {
 	alert("🧙‍♂️ J'arrive, j'arrive Aventurier !");
 });
+
+// Exercice 5 : Faisons un peu de magie : 
+
+const boxes = document.querySelectorAll('#boites_magique .boite')
+
+const redButton = document.querySelector('#btn_change_red');
+const blueButton = document.querySelector("#btn_change_blue");
+const greenButton = document.querySelector("#btn_change_green");
+
+redButton.addEventListener("click", () => {
+	boxes[0].style.backgroundColor = 'red';
+})
+
+blueButton.addEventListener("click", () => {
+	boxes[0].style.backgroundColor = "blue";
+	boxes[1].style.backgroundColor = "blue";
+});
+
+
+greenButton.addEventListener("click", () => {
+	boxes[0].style.backgroundColor = "green";
+	boxes[1].style.backgroundColor = "green";
+	boxes[2].style.backgroundColor = "green";
+});
+
