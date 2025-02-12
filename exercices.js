3b6abaf68d876302acb1a0693e003113021bcfce
Lucas Olivarez <74412774+novaenebulae@users.noreply.github.com>
2025-02-12 09:28:38 +0100

    Add exercice 4

--- exercices.js
+++ exercices.js
@@ -1,14 +1,14 @@
-// Exercice 1 : Quel est le titre de la boutique : 
+// Exercice 1 : Quel est le titre de la boutique :
 
-const mainTitle = document.querySelector('h1');
+const mainTitle = document.querySelector("h1");
 
 console.log(mainTitle.textContent);
 
-// Exercice 2 : Des informations manquent ! : 
+// Exercice 2 : Des informations manquent ! :
 
-const descriptionDiv = document.getElementById('description_boutique');
+const descriptionDiv = document.getElementById("description_boutique");
 
-const descriptionElement = document.createElement('p')
+const descriptionElement = document.createElement("p");
 descriptionElement.textContent = "Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l'acheter.";
 descriptionDiv.appendChild(descriptionElement);
 
@@ -16,3 +16,10 @@ descriptionDiv.appendChild(descriptionElement);
 
 const blagueRoger = document.getElementById("blague_de_roger_le_sorcier");
 blagueRoger.remove();
+
+// Exercice 4 : Archibald n'est pas là, appelons le ! :
+
+const callButton = document.getElementById("call_archibald");
+callButton.addEventListener("click", () => {
+	alert("🧙‍♂️ J'arrive, j'arrive Aventurier !");
+});
